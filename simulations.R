8843c0e355d313868d7570d7993113e383c44c7d
Omid S Solari <omid@shiraz.lbl.gov>
2018-09-20 16:13:04 -0700

    add

--- simulations.R
+++ simulations.R
@@ -1,11 +1,10 @@
 require(ggplot2)
 source("simulations.lib.R")
 
-out2s <- simErrorRatio(kappa = .01) 
+out <- simErrorRatio(n = seq(1, 1501, length.out = 31), kappa = .01) 
 
-ggplot() + geom_point(aes(x = out$n, y = out$PRatio)) + geom_smooth(aes(x = out$n, y = out$PRatio), method = "lm")
-
+ggplot() + geom_point(aes(x = out2s$n, y = out2s$PRatio)) + geom_smooth(aes(x = out2s$n, y = out2s$PRatio), method = "lm")
 
 ggplot() + geom_point(aes(x = out2$n, y = out2$PRatio)) + geom_smooth(aes(x = out2$n, y = out2$PRatio), method = "lm")
 
-summary(lm(out2$PRatio ~ out2$n))
+summary(lm(out2s$PRatio ~ out2s$n))
